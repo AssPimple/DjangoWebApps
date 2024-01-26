aacf437e3b85962dbc8b35c91cc6756fdb05128f
Pugach Dmitriy <07700660gg@mail.ru>
2024-01-26 16:08:30 +0500

    users

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
+
 
 ]
 
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
